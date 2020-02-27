8e601462f3bb4987760ba2996e241dbea6b0c9f1
jose-marquez89 <jlmarquez89@gmail.com>
2020-02-27 10:26:33 -0600

    added heroku config var to app.py

--- TwitOff/app.py
+++ TwitOff/app.py
@@ -15,7 +15,7 @@ def create_app():
     """Create and configure an instance of Flask"""
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = SQLITE_PATH
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
